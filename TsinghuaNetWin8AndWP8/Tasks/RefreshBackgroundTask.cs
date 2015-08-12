404e5a6a7b889ff9bedab346e66fd332989148b8
刘正元 <lzy@live.in>
2015-08-12 14:20:26 +0000

    new win8 version

--- TsinghuaNetWin8AndWP8/Tasks/RefreshBackgroundTask.cs
+++ TsinghuaNetWin8AndWP8/Tasks/RefreshBackgroundTask.cs
@@ -1,59 +1,23 @@
-﻿using Windows.ApplicationModel.Background;
-using Windows.Data.Xml.Dom;
-using Windows.Storage;
-using Windows.UI.Notifications;
-using Windows.Web.Http;
-using System;
-using System.Text.RegularExpressions;
+﻿using System;
 using System.Globalization;
+using System.Linq;
+using Windows.ApplicationModel.Background;
 using Windows.ApplicationModel.Resources;
-using System.Threading.Tasks;
+using Windows.Data.Xml.Dom;
+using Windows.Networking.Connectivity;
+using Windows.UI.Notifications;
 
 namespace Tasks
 {
     public sealed class RefreshBackgroundTask : IBackgroundTask
     {
-        private readonly string userName = (string)ApplicationData.Current.RoamingSettings.Values["UserName"];
-        private readonly string passwordMd5 = (string)ApplicationData.Current.RoamingSettings.Values["PasswordMD5"];
+        private readonly string userName;
+        private readonly string passwordMD5;
 
         private readonly string logOnSucessful;
         private readonly string used;
 
-        /// <summary>
-        /// 登陆网络。
-        /// </summary>
-        /// <exception cref="System.InvalidOperationException">在登陆过程中发生错误。</exception>
-        /// <returns>是否发生登陆。</returns>
-        private async Task<bool> logOn()
-        {
-            using(var http = new HttpClient())
-            {
-                string res = null;
-                Func<string, Task<bool>> check = async toPost =>
-                {
-                    try
-                    {
-                        res = await http.PostStrAsync(new Uri("http://net.tsinghua.edu.cn/cgi-bin/do_login"), toPost);
-                    }
-                    catch(AggregateException)
-                    {
-                        return false;
-                    }
-                    if(Regex.IsMatch(res, @"^\d+,"))
-                    {
-                        var a = res.Split(',');
-                        traffic = new Size(ulong.Parse(a[2], CultureInfo.InvariantCulture));
-                        return true;
-                    }
-                    return false;
-                };
-                if(await check("action=check_online"))
-                    return false;
-                return await check("username=" + userName + "&password=" + passwordMd5 + "&mac=" + MacAddress.Current + "&drop=0&type=1&n=100");
-            }
-        }
-
-        private Size traffic;
+        private bool run;
 
         public RefreshBackgroundTask()
         {
@@ -69,16 +33,49 @@ namespace Tasks
             var l = ResourceLoader.GetForViewIndependentUse("Tasks/Resources");
             used = l.GetString("Used");
             logOnSucessful = l.GetString("LogOnSucessful");
+
+            //初始化信息存储区
+            try
+            {
+                var passVault = new Windows.Security.Credentials.PasswordVault();
+                var pass = passVault.FindAllByResource("TsinghuaAccount").First();
+                pass.RetrievePassword();
+                userName = pass.UserName;
+                passwordMD5 = pass.Password;
+                run = true;
+            }
+            // 未找到储存的密码
+            catch(Exception ex) when (ex.HResult == -2147023728)
+            {
+                run = false;
+            }
         }
 
         #region IBackgroundTask 成员
 
         public async void Run(IBackgroundTaskInstance taskInstance)
         {
+            if(!run)
+                return;
+            var connection = NetworkInformation.GetInternetConnectionProfile();
+            if(connection == null)
+                return;
+            if(connection.IsWwanConnectionProfile)
+                return;
             var d = taskInstance.GetDeferral();
-            if(await logOn())
-                SendToastNotification(logOnSucessful, string.Format(CultureInfo.CurrentCulture, used, traffic));
-            d.Complete();
+            var client = new Web.WebConnect(userName, passwordMD5);
+            try
+            {
+                await client.LogOnAsync();
+                await client.RefreshAsync();
+                if(!client.IsOnline)
+                    return;
+                SendToastNotification(logOnSucessful, string.Format(CultureInfo.CurrentCulture, used, client.WebTrafficExact));
+            }
+            finally
+            {
+                d.Complete();
+            }
         }
 
         #endregion
