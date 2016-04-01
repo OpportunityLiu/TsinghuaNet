7524700a7b42bf01c457ac93b90e805274e52200
刘正元 <lzy@live.in>
2016-04-01 17:17:13 +0000

    sslvpn page

---
+++ TsinghuaNetUAP/TsinghuaNet/WebPages/Data.lang-en.js
@@ -0,0 +1,7 @@
+﻿document.all(0).lang = "en";
+document.title = "New Tab";
+
+setLocalName("learn", "Web learning");
+setLocalName("info", "Info");
+setLocalName("its", "Info tech service");
+setLocalName("sslvpn", "Off-campus access");
---
+++ TsinghuaNetUAP/TsinghuaNet/WebPages/Data.lang-zh-cn.js
@@ -0,0 +1,7 @@
+﻿document.all(0).lang = "zh-cn";
+document.title = "新标签页";
+
+setLocalName("learn", "网络学堂");
+setLocalName("info", "信息门户");
+setLocalName("its", "信息化服务");
+setLocalName("sslvpn", "校外访问");
